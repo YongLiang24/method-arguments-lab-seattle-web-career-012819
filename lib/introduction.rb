0c54691d43c8e60ea72450b6a7c87d28caf6095a
yong liang <yongliang24@gmail.com>
2019-01-18 21:37:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language (name1, name2)
-  puts "Hi, my name is #{name1} and I am learning to program in #{name2}.
+  puts "Hi, my name is #{name1} and I am learning to program in #{name2}."
 end
